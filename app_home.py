cb104118a57a106fb5d62d4b88a649162b138867
TaeUkChu <62568938+TaeUkChu@users.noreply.github.com>
2023-02-21 19:12:04 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -9,8 +9,8 @@ def load_lottieurl(url: str):
     return r.json()
 
 def app_home():
-    st.header("1조 pose_tracking_project")
+    st.header("프로젝트 \"DD\" 메인화면입니다.")
     lottie_url = "https://assets4.lottiefiles.com/packages/lf20_3dw8ed6q.json"
     lottie_json = load_lottieurl(lottie_url)
-    st.subheader("프로젝트 \"DD\" 메인화면입니다.")
+    
     st_lottie(lottie_json)
